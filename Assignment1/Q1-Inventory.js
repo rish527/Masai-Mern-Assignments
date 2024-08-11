da6cea67c283487d1b53ffe5c69da51568aba26e
Asus <rishav032005@gmail.com>
2024-08-11 10:42:29 +0530

    First Commit

---
+++ Assignment1/Q1-Inventory.js
@@ -0,0 +1,85 @@
+function Item(id, name, quant, price){
+    this.id=id;
+    this.name=name;
+    this.quant=quant;
+    this.price=price;
+}
+
+//Adding new Items
+function addItem(inventory, item){
+    inventory.push(item);
+}
+
+function updateItem(inventory,id, newDetails){
+    const itemIndex=inventory.findIndex((item)=>item.id===id);
+    if(itemIndex!=-1){
+        inventory[itemIndex]={...inventory[itemIndex], ...newDetails};
+    }
+    else{
+        console.log(`Item with id ${id} not found`);
+    }
+
+}
+
+
+function deleteItem(inventory, id){
+    const itemIndex=inventory.findIndex((obj)=>obj.id===id);
+    if(itemIndex!=-1){
+        inventory.splice(itemIndex,1);
+    }
+    else{
+        console.log(`Item with id ${id} not found`)
+    }
+}
+
+
+function getItem(inventory, id){
+    const itemIndex=inventory.findIndex((item)=>item.id===id);
+    if(itemIndex!=1){
+        return inventory[itemIndex];
+    }
+    else{
+        console.log(`Item with id ${id} not found`);
+    }
+}
+
+function printInventory(inventory){
+    inventory.forEach(element => {
+        console.log(`
+           Id:${element.id},
+           Name:${element.name},
+           Quantity:${element.quant},
+           Price:${element.price}
+        `)
+    })
+        
+};
+
+
+const inventory = [];
+
+// Adding items
+addItem(inventory, new Item(1, 'Apple', 50, 0.5));
+addItem(inventory, new Item(2, 'Banana', 30, 0.3));
+addItem(inventory, new Item(3, 'Orange', 20, 0.7));
+
+
+console.log("Main Inventory:")
+printInventory(inventory);
+
+// Updating an item
+updateItem(inventory, 2, { name: 'Green Banana', quantity: 25 });
+
+
+console.log("Inventory after update:")
+printInventory(inventory);
+
+
+const item = getItem(inventory, 3);
+console.log('Retrieved Item:', item);
+
+
+deleteItem(inventory, 1);
+
+console.log("After deletion:");
+printInventory(inventory);
